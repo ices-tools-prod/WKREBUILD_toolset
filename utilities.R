7a6d9c2eabce4e8539ed5c1833073d91fb69e215
Iago Mosqueira <iago@mosqueira.eu>
2023-03-15 12:39:09 +0100

    Deleted unused function
    Former-commit-id: baa9307538b451e6963a67dbb5150e7bb67287a3 [formerly c00076b9d0dee2c844dc726dd42f1e42304ca65d]
    Former-commit-id: 1bbe263263e5f78e5092f261fcd1332dded6c774

--- utilities.R
+++ utilities.R
@@ -6,48 +6,6 @@
 #
 # Distributed under the terms of the EUPL-1.2
 
-
-# retroErrorByAge {{{
-
-#' @param retros
-#' @param object
-
-retroErrorByAge(retros, stock.n(om))
-
-names(retroErrorByAge(retros, stock.n(om)))
-
-retroErrorByAge <- function(retros, object) {
-browser()
-  # GET dims
-  nret <- length(retros) - 1
-  fy <- dims(retros[[1]])$maxyear
-
-  # COMPUTE errors: N=stock.n, F=harvest
-  dat <- do.call(rbind, lapply(seq(2, nret), function(y) {
-  model.frame(FLQuants(
-    N=stock.n(retros[[1]])[, ac(seq(fy - nret, fy - y + 1))] /
-      stock.n(retros[[y]])[, ac(seq(fy - nret, fy - y + 1))],
-    F=harvest(retros[[1]])[, ac(seq(fy - nret, fy - y + 1))] /
-      harvest(retros[[y]])[, ac(seq(fy - nret, fy - y + 1))]))
-  }))
-
-  # CALCULATE covariance with age
-  cab <- cov(dat[, c('age', 'N')])
-
-  # COMPUTE mean and sd by age
-  musig <- data.table(dat)[, .(mean=mean(N), sd=sd(N)), by=age]
-
-  nsamp <- length(object[1,])
-
-  out <- Reduce(qbind, FLQuants(lapply(musig$age, function(x) {
-    FLQuant(rnorm(nsamp, musig[age == x]$mean, musig[age == x]$sd),
-      dimnames=list(age=x, year=dimnames(object)$year))
-  })))
-
-  return(out)
-}
-# }}}
-
 # icesControl {{{
 
 #' Creates an `mpCtrl` object for the standard ICES MP evaluation
