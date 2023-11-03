6281e7d66a11eb0782e00ecc10db33750d636f25
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2023-11-03 17:29:43 +0100

    New README with tutorial

--- report.R
+++ report.R
@@ -39,6 +39,13 @@ plot(window(om, end=2023)) +
   ggtitle("sol.27.4 OM")
 dev.off()
 
+
+# DEVIANCES
+taf.png("om_deviances.png")
+plot(deviances(om)) +
+  ggtitle("SRR deviances ~ LNAR1(0, sigmaR, rho)")
+dev.off()
+
 # OM /refpts
 taf.png("om_icesmetrics.png")
 plot(window(om, end=2023), metrics=icesmetrics) +
@@ -54,7 +61,8 @@ load("model/model.rda")
 # BASELINE run: F=0
 taf.png("run_f0.png")
 plot(runf0) +
-  geom_vline(xintercept=2023, linetype=3)
+  geom_vline(xintercept=2023.5, linetype=3) +
+  ggtitle("fwd(F=0), 2024-2042")
 dev.off()
 
 # ADVICE rule run
@@ -63,10 +71,20 @@ plot(om, advice, metrics=icesmetrics) +
   geom_hline(yintercept=1, linetype=2, alpha=0.5)
 dev.off()
 
+# PLOT AR hockeystick
+
+taf.png("advice_hcr.png")
+plot_hockeystick.hcr(control(advice)$hcr,
+  labels=c(trigger="MSYBtrigger", limit="", min="", target="Ftarget")) +
+  xlab("SSB") + ylab("F")
+dev.off()
+
+
 # PLOT RUNS
 taf.png("plans.png")
 plot(runf0, plans, window=FALSE) +
-  geom_vline(xintercept=2023, linetype=3)
+  geom_vline(xintercept=2023, linetype=3) +
+  ggtitle("F=0 + MPs")
 dev.off()
 
 taf.png("plans_fr.png")
@@ -82,6 +100,9 @@ Reduce('+', Map(function(x, y)
   xlab("SSB") + ylab(expression(bar(F))) +
   ggtitle(y), x=plans[1:4], y=names(plans[1:4])))
 dev.off()
+
+tracking(advice)[c('SB.om', 'met.hcr', 'decision.hcr', 'hcr',
+  'fbar.isys', 'isys'), ac(2024:2026)]
 
 # --- Performance (output.R)
 
@@ -104,6 +125,13 @@ plotBPs(perf[year=='medium'], statistics=c("AAVC", "C", "risk2")) +
 taf.png("perf_tos.png")
 plotTOs(perf[year=='short'], x="C", y=c("AAVC", "risk2"))
 dev.off()
+
+# PLOT om + plan ssb
+taf.png("onruns.png")
+plotOMruns(window(ssb(om), end=2023), FLQuants(lapply(plans, ssb))) +
+  ggtitle("SSB (t)")
+dev.off()
+
 
 # PLOT PBlim by year and mp
 
